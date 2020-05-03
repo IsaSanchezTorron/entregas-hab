9750819bba98127f50b59ea5f8f46d819634b6c5
IsaSanchezTorron <isasancheztorron@gmail.com>
2020-05-03 23:03:26 +0200

    23:03

--- Backend/Entrega2/ejercicio2.js
+++ Backend/Entrega2/ejercicio2.js
@@ -11,9 +11,11 @@ console.log("Memoria libre:" + os.freemem() + " bytes");
 porcentajeMemoria = parseInt((os.totalmem() / os.freemem()) * 100);
 console.log(`Tienes un ${porcentajeMemoria} % de memoria libre `);
 
+//--------------------------------------------
+
 let x = os.totalmem();
 let y = os.freemem();
-let z = (100 * y) / x;
+let z = (100 * x) / y;
 console.log(z);
 
 // No consigo encontrar la lógica del porcentaje :(
