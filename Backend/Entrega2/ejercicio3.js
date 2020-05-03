06446ab4582075ceee1516a44ee91a900ecf192d
IsaSanchezTorron <isasancheztorron@gmail.com>
2020-05-04 00:12:34 +0200

    00:12

--- Backend/Entrega2/ejercicio3.js
+++ Backend/Entrega2/ejercicio3.js
@@ -2,3 +2,18 @@
 // ############################## Hacer un servidor web que responda a la url / con un texto que ponga ###########################
 // #################### "Hola" pero sólo por la mañana, el resto del día debería devolver una página de error 404. ###############
 // ###############################################################################################################################
+
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.listen(process.env.PORT);
+
+app.use(function (req, res) {
+  if (new Date().getHours() > 8 && new Date().getHours() < 12) {
+    res.end("Hola");
+  } else {
+    res.statusCode = 404;
+    res.end("Finiquito");
+  }
+});
